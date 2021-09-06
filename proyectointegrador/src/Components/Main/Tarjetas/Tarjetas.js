798e13ce2de6ebac435932ef2fd96f46bd7986af
Laura Rolon <mrolonmartinez@udesa.edu.ar>
2021-09-06 11:39:21 -0300

    componentes

--- proyectointegrador/src/Components/Main/Tarjetas/Tarjetas.js
+++ proyectointegrador/src/Components/Main/Tarjetas/Tarjetas.js
@@ -1,1 +1,21 @@
-//aca
+import React from 'react';
+
+function Tarjetas() {
+    return (
+        <div>
+            <main>
+                <img src="./img/image-default.png" alt=""/>
+                <h3>Título/ Nombre</h3>
+                <p class ="description">Lorem ipsum dolor sit amet consectetur adipisicing elit.Sint cumque velit minus facere laboriosam voluptatem impedit ea unde labore optio eius quis, dignissimos expedita.Culpa, soluta perspiciatis!Sint, laboriosam cum.</p>
+                <section class ="aditional-info">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Esse qui atque.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Esse qui atque.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Esse qui atque.</p>
+                </section>
+                <a href="">Ver más</a>
+            </main>
+        </div>
+    );
+}
+
+export default Tarjetas;
