f3cead72cc81a1f0259721dc2d9f5ebc27bbe9ad
mariapaulaherrera <mpherrera@udesa.edu.ar>
2021-09-09 10:49:06 -0300

    header

--- proyectointegrador/src/Components/Header/Header.js
+++ proyectointegrador/src/Components/Header/Header.js
@@ -1,17 +1,30 @@
 import React from "react";
-//import './header.css'
+import './header.css'
 
 function Header(){
     return(
         <header className="header">
             <section className="seccionHeader">
-                <img className="logo" src="./assets/img/image-default.png" alt=""></img> 
-                <form action="">
-                    <div class="divInput">
-                        <input class="inputBuscador" type="text" name="busqueda" placeholder="Buscar..." />
-                    </div>
-                    <button class="botonSearch" type="submit"><span id="lupa" href="" uk-icon="search"></span></button>
-                </form>
+                <li className="logotipo">
+                    <img className="logo" src="./assets/img/image-default.png" alt=""/>
+                </li>
+                <li className="titulosHeader">
+                    <a className="textoHeader">Inicio</a>
+                </li>
+                <li className="titulosHeader">
+                    <a className="textoHeader">Películas</a>
+                </li>
+                <li className="titulosHeader">
+                    <a className="textoHeader">Series</a>
+                </li>
+                <li className="titulosHeader">
+                    <form action="">
+                        <div className="divInput">
+                            <input className="inputBuscador" type="text" name="busqueda" placeholder="Buscar..." />
+                            <button className="botonSearch" type="submit"></button>
+                        </div>
+                    </form>
+                </li>
             </section>
         </header>
     )
