65f90f0fa6bd04f4846aee8d6209fa9a47b4d472
mariapaulaherrera <mpherrera@udesa.edu.ar>
2021-09-13 09:20:37 -0300

    componente App

--- proyectointegrador/src/App.js
+++ proyectointegrador/src/App.js
@@ -1,15 +1,26 @@
 import Header from "./Components/Header/Header"
 import Footer from "./Components/Footer/Footer";
 import MainContent from "./Components/Main/MainContent";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component() {
+  constructor(props){
+    super(props)
+    this.state = {
+      valor: ""
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
       
-      <MainContent />
-      <Footer />
-    </div>
+        <MainContent />
+        <Footer />
+      </div>
   );
+  }
+  
 }
 
 export default App;
